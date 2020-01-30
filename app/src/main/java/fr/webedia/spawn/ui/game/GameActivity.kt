fc4d9483b79ab5c90900e3f0f53b4becb0008dca
Anne-Claire Femia <anne-claire.femia@webedia-group.com>
2020-01-30 16:14:55 +0100

    [add] game details activity

---
+++ app/src/main/java/fr/webedia/spawn/ui/game/GameActivity.kt
@@ -0,0 +1,15 @@
+package fr.webedia.spawn.ui.game
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.webedia.spawn.R
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+}
