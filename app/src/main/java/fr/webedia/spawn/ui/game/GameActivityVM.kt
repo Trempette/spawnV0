0d5b29068eb90804da280e25f5d0c184f31966c1
ekerhir <edouard.kerhir@webedia-group.com>
2020-01-30 17:11:44 +0100

    to pull

--- app/src/main/java/fr/webedia/spawn/ui/game/GameActivityVM.kt
+++ app/src/main/java/fr/webedia/spawn/ui/game/GameActivityVM.kt
@@ -5,9 +5,11 @@ import android.view.MenuItem
 import androidx.annotation.NonNull
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.map
 import com.facebook.ads.internal.id
 import com.webedia.optimusprime.utils.livedata.SingleLiveEvent
 import fr.webedia.spawn.ui.common.BaseViewModel
+import fr.webedia.spawn.utils.DateUtil
 import fr.webedia.spawn.utils.InjectorUtils
 
 class GameActivityVMFactory(val application: Application, id: String) : ViewModelProvider.Factory {
@@ -16,9 +18,23 @@ class GameActivityVMFactory(val application: Application, id: String) : ViewMode
     }
 }
 
-class GameActivityVM(application: Application, id: String) : BaseViewModel(application) {
+class GameActivityVM(application: Application, val id: String) : BaseViewModel(application) {
 
     val game = InjectorUtils.provideRepository(application.applicationContext).getGameById(id)
 
+    val releaseDate = game.map {
+        DateUtil.getYearMonthandDay(it.releaseDate)
+    }
 
+    val posterUrl = game.map {
+        it.imageUrl
+    }
+
+    val genre = game.map {
+        it.genre
+    }
+
+    val editor = game.map {
+        it.editor
+    }
 }
