0a71807ca8ba3a8ca04ba6f098e58ca7dbd0bba3
Anne-Claire Femia <anne-claire.femia@webedia-group.com>
2020-01-30 12:52:00 +0100

    [update] remove mainactivity launch

--- app/src/main/java/fr/webedia/spawn/ui/splash/SplashActivity.kt
+++ app/src/main/java/fr/webedia/spawn/ui/splash/SplashActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.webedia.spawn.R
 import fr.webedia.spawn.ui.home.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Thread.sleep(2000)
-        launchMainActivity()
+        //launchMainActivity()
     }
 
     private fun launchMainActivity() {
