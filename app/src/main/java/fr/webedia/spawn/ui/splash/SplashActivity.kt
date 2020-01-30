a48767870375e24cfbe0e0297bb177813c3e493e
ekerhir <edouard.kerhir@webedia-group.com>
2020-01-30 13:49:07 +0100

    [add] delay for splash

--- app/src/main/java/fr/webedia/spawn/ui/splash/SplashActivity.kt
+++ app/src/main/java/fr/webedia/spawn/ui/splash/SplashActivity.kt
@@ -1,11 +1,13 @@
 package fr.webedia.spawn.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import fr.webedia.spawn.R
 import fr.webedia.spawn.ui.home.MainActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
@@ -13,7 +15,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //launchMainActivity()
+        Handler().postDelayed({
+            launchMainActivity()
+        }, 2000)
     }
 
     private fun launchMainActivity() {
